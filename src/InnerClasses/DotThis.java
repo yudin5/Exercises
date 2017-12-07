e8d0921eb4ed3cd1b925127c46c636e0bbe1d3f8
Vitaly Yudin <vyudin@tsconsulting.ru>
2017-12-07 11:17:57 +0300

    Add to GIT

--- src/InnerClasses/DotThis.java
+++ src/InnerClasses/DotThis.java
@@ -3,17 +3,21 @@ package InnerClasses;
 //: innerclasses/DotThis.java
 // Обращение к объекту внешнего класса.
 public class DotThis {
+
     void f() { System.out.println("DotThis.f()"); }
+
+    public Inner inner() { return new Inner(); }
+
+    public static void main(String[] args) {
+        DotThis dt = new DotThis();
+        DotThis.Inner dti = dt.inner();
+        dti.outer().f();
+    }
+    ////////////////////////////////////////////////
     public class Inner {
         public DotThis outer() {
             return DotThis.this;
             // A plain "this" would be Inner's "this"
         }
-    }
-    public Inner inner() { return new Inner(); }
-    public static void main(String[] args) {
-        DotThis dt = new DotThis();
-        DotThis.Inner dti = dt.inner();
-        dti.outer().f();
     }
 }
