dbf6ff567fd50d8a9bffbc4995ee7f2fc7d6a078
Vitaly Yudin <vyudin@tsconsulting.ru>
2017-12-15 16:55:16 +0300

    Add to GIT

--- src/Parametrization/SuperAndExtends.java
+++ src/Parametrization/SuperAndExtends.java
@@ -5,7 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SuperAndExtends {
+
+    static void test(int x, int y) {}
+    //static void test(int x, int y) {}
+    static void test(Integer x, Integer y) {}
+
     public static void main(String[] args) {
+        Float[] arrF = new Float[2];
+        for (float f : arrF) {   // NullPointerException !!!
+            System.out.println(f);
+        }
+        long l4 = 200;
+        int i4 = (int) l4;
+        boolean x3 = false;
+        boolean y3 = true;
+        while(y3 = x3) {}
+        int x1 = 1, y1 = 2, z1 = 3;
+        System.out.println("test" + (x1 + y1));
+        byte g = (byte) 300;
+        Float d2 = 4.0f;
+        float f2 = 4.0f;
+        System.out.println(d2 == f2);
+        System.out.println((double) 3/7);
+        boolean b2 = true;
+        System.out.println("10" + b2);
+        System.out.println(Float.NaN);
+        byte x = 5;
+        char y = 10;
+        test(x, y);
         char ch = 65;
         System.out.println(ch);
         Integer N1 = 100;
