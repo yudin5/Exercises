fc75e2d8003a7676aa0fb8d3cfcb02e058f22e7d
Vitaly Yudin <vyudin@tsconsulting.ru>
2017-12-25 15:10:45 +0300

    Add to GIT

--- src/OverrideClasses/PrivateOverride.java
+++ src/OverrideClasses/PrivateOverride.java
@@ -15,12 +15,19 @@ public class PrivateOverride {
     public static void main(String[] args) {
         PrivateOverride po = new Derived();
         po.f(6); // Вызов родительского приватного метода.
-        po.test(); // Вызов родительского метода. По типу ссылки.
+        po.test(); // Вызов СТАТИЧЕСКОГО родительского метода. По типу ссылки.
+        System.out.println(po.getNumber()); // 4. Вызов метода по типу ОБЪЕКТА. Позднее связывание. Метод
+        // НЕ статический
+        Derived derived = new Derived();
+        //derived.test();
     }
 }
 
 class Derived extends PrivateOverride {
-    public byte getNumber() { return 4;}
+    @Override
+    public byte getNumber() { return 4; }
+    public int getNumber(int y) { return 10;}
+
     static void test() {
         System.out.println("Child static");
     }
